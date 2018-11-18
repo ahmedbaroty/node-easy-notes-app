d8af3493105ae613fd2eb6324fc1dd8b8c2ef369
ABD EL-FATAH Ahmed Ext OBS/CSO <dqxz4195@orange.com>
2018-11-18 14:37:11 +0200

    config the database

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+// express
 const express = require('express');
 const bodyParser = require('body-parser');
+
+// configuration the database
+const dbConfig = require('./config/database.config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// connecting to the database
+mongoose.connect(dbConfig.url, {useNewUrlParser: true}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(error => {
+    console.log('Could not connect to the database. Exiting now...', error);
+    process.exit();
+});
 
 // create express app
 const app = express();
@@ -11,12 +26,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // define a simple route
-app.get('/' , (req , res) => {
-   res.json({message : 'Welcome To EasyNote application'})
+app.get('/', (req, res) => {
+    res.json({message: 'Welcome To EasyNote application'})
 });
 
 // listen for requests
-app.listen(3000 , ()=>{
-   console.log("Server is listening on port 3000")
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000")
 });
 
