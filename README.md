7014c378a76cb85438ab0cf392640f381b327abc
ABD EL-FATAH Ahmed Ext OBS/CSO <dqxz4195@orange.com>
2018-11-18 14:25:12 +0200

    adding npm dependacies and simple route example

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-"# node-easy-notes-app" 
-"# node-easy-notes-app" 
+# node-easy-notes-app
+- Install dependencies
+    - We will need express, mongoose and body-parser modules in our application. Let’s install them by typing the following command
+      
+      ````$ npm install express body-parser mongoose --save````
+      
+    -   
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// create express app
+const app = express();
+
+// parser requests of content-type application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// parser requests of content-type - application/json
+app.use(bodyParser.json());
+
+// define a simple route
+app.get('/' , (req , res) => {
+   res.json({message : 'Welcome To EasyNote application'})
+});
+
+// listen for requests
+app.listen(3000 , ()=>{
+   console.log("Server is listening on port 3000")
+});
+
